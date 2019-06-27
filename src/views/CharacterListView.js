c3343bf04feacd5ad853c6e654efacc987e6e952
Eric Hendrix <agentmrbig@gmail.com>
2019-06-27 04:13:02 -0400

    got rid of console errors, setup actions, can see success via logger in console

--- src/views/CharacterListView.js
+++ src/views/CharacterListView.js
@@ -3,19 +3,24 @@ import { connect } from "react-redux";
 
 import { CharacterList } from "../components";
 // import actions
+import { getCharacters } from "../actions";
 
 class CharacterListView extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
   }
 
   componentDidMount() {
     // call our action
+    this.props.getCharacters();
   }
 
   render() {
     if (this.props.fetching) {
       // return something here to indicate that you are fetching data
+      return (
+        <span>Looking for Star Wars characters!</span>
+      )
     }
     return (
       <div className="CharactersList_wrapper">
@@ -25,11 +30,21 @@ class CharacterListView extends React.Component {
   }
 }
 
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    characters: state.charsReducer.characters,
+    error: state.charsReducer.error,
+    fetching: state.charsReducer.fetching
+  }
+}
+
 // our mapStateToProps needs to have two properties inherited from state
 // the characters and the fetching boolean
 export default connect(
-  null /* mapStateToProps replaces null here */,
+  mapStateToProps /* mapStateToProps replaces null here */,
   {
     /* action creators go here */
+    getCharacters
   }
 )(CharacterListView);
